386145eac3f18fffa8d860fb0174be7ddd68378d
akashtripathi1 <120573423+akashtripathi1@users.noreply.github.com>
2024-05-26 21:42:06 +0530

    Feat: toggle button in navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,42 +1,70 @@
-import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Divider } from '@mui/material';
-// import DashboardIcon from '@mui/icons-material/Dashboard';
-// import InventoryIcon from '@mui/icons-material/Inventory';
-// import AssignmentIcon from '@mui/icons-material/Assignment';
+import React, { useState } from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, IconButton, Divider } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
+const miniDrawerWidth = 56;
 
 const Navbar = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <Drawer
       variant="permanent"
+      open={open}
       sx={{
-        width: drawerWidth,
+        width: open ? drawerWidth : miniDrawerWidth,
         flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        transition: theme => theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        [`& .MuiDrawer-paper`]: { 
+          width: open ? drawerWidth : miniDrawerWidth, 
+          boxSizing: 'border-box', 
+          overflowX: 'hidden'
+        },
       }}
     >
-      <Toolbar />
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <IconButton onClick={handleDrawerToggle}>
+          {open ? <ChevronLeftIcon /> : <MenuIcon />}
+        </IconButton>
+      </Toolbar>
       <Divider />
       <List>
-        <ListItem button component={Link} to="/dashboard">
+        <ListItem component={Link} to="/dashboard">
           <ListItemIcon>
-            {/* <DashboardIcon /> */}
+            <DashboardIcon />
           </ListItemIcon>
-          <ListItemText primary="Dashboard" />
+          {open && <ListItemText primary="Dashboard" />}
         </ListItem>
-        <ListItem button component={Link} to="/assets">
+        <ListItem component={Link} to="/assets">
           <ListItemIcon>
-            {/* <InventoryIcon /> */}
+            <InventoryIcon />
           </ListItemIcon>
-          <ListItemText primary="Assets" />
+          {open && <ListItemText primary="Assets" />}
         </ListItem>
-        <ListItem button component={Link} to="/tickets">
+        <ListItem component={Link} to="/tickets">
           <ListItemIcon>
-            {/* <AssignmentIcon /> */}
+            <AssignmentIcon />
           </ListItemIcon>
-          <ListItemText primary="Tickets" />
+          {open && <ListItemText primary="Tickets" />}
         </ListItem>
       </List>
     </Drawer>
