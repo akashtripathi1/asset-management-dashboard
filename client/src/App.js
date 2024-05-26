e50c48d962174bf5892e2bfe3c418263496ca534
akashtripathi1 <120573423+akashtripathi1@users.noreply.github.com>
2024-05-27 00:32:31 +0530

    added context to app.js and changed component location

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,38 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { CssBaseline, Box } from '@mui/material';
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/pages/Dashboard';
-import Assets from './components/pages/Assets';
-import Tickets from './components/pages/Tickets';
+import Dashboard from './components/Dashboard/Dashboard';
+import Assets from './components/Assets/Assets';
+import Tickets from './components/Tickets/Tickets';
+import AssetState from './context/assets/AssetState';
+import TicketState from './context/tickets/TicketState';
 import './App.css';
 
 function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Router>
-        <Navbar />
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, p: 3 }}
-        >
-          <Routes>
-            <Route exact path="/" element={<Navigate to="/dashboard" />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/assets" element={<Assets />} />
-            <Route path="/tickets" element={<Tickets />} />
-          </Routes>
+    <AssetState>
+      <TicketState>
+
+
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <Router>
+            <Navbar />
+            <Box
+              component="main"
+              sx={{ flexGrow: 1, p: 3 }}
+            >
+              <Routes>
+                <Route exact path="/" element={<Navigate to="/dashboard" />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/assets" element={<Assets />} />
+                <Route path="/tickets" element={<Tickets />} />
+              </Routes>
+            </Box>
+          </Router>
         </Box>
-      </Router>
-    </Box>
+      </TicketState>
+    </AssetState>
   );
 }
 
